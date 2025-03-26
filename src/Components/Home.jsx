863e1aaf36cfb37e7b5f88b3e6e644221aac7e5b
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-03-26 16:32:26 +0100

    fixed the build failed error by correcting import Main

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from "react";
 import Category from './Category';
 import { data } from 'react-router-dom';
-import Main from './main';
+import Main from './Main';
 
 function Home() {
     const [catId, setCatId] = useState("");
